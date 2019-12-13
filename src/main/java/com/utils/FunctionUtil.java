801942645e44f75b2cc35ded1642466fe69fc06a
leo <272137499@qq.com>
2019-12-13 23:20:21 +0800

    函数化处理新增#{__md5}处理判断

--- src/main/java/com/utils/FunctionUtil.java
+++ src/main/java/com/utils/FunctionUtil.java
@@ -1,5 +1,8 @@
 package com.utils;
 
+import org.apache.commons.codec.cli.Digest;
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +16,7 @@ public class FunctionUtil {
     static String pattern = "\\#\\{(.+?)\\}";
     static String test1 = "#{__time}";
     static String test2 = "#{__UUID}";
+    static String test3= "#{__md5(abc,1234)}";
 
     /**
      * @param func
@@ -24,8 +28,7 @@ public class FunctionUtil {
         while (matcher.find()){
             String toReplace = matcher.group();
             String replaceKey = matcher.group(1);
-            System.out.println(toReplace + " + " + replaceKey);
-            func = func.replace(toReplace, getFunction(replaceKey));
+            func = func.replace(toReplace, getFunctions(replaceKey));
         }
         System.out.println(func);
         return func;
@@ -34,18 +37,43 @@ public class FunctionUtil {
     /**
      *  识别并返回函数处理内容
      */
-    public static String getFunction(String func){
+    public static String getFunctions(String func){
         // 大小写忽略
         if ("__uuid".equalsIgnoreCase(func)){
             return UUID.randomUUID().toString().replace("-","");
         }else if("__time".equalsIgnoreCase(func)){
             // long型返回为字符串处理
             return "" + System.currentTimeMillis();
+        }else if(func.startsWith("__md5")){
+            String[] args = getArgs(func);
+            String md5 = "";
+            // 先判断参数
+            if (args.length > 1) {
+                md5 = DigestUtils.md5Hex(args[0] + args[1]);
+            }
+            return md5;
         }
         return func;
     }
 
+    /**
+     * @param func
+     * @return 返回函数化参数数组
+     */
+    static String[] getArgs(String func){
+        String regex = "\\((.+?)\\)";
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(func);
+        String[] split = null;
+        while (matcher.find()){
+            String group = matcher.group(1);
+            split = group.split(",");
+        }
+        return split;
+
+    }
+
     public static void main(String[] args) {
-        replace(test1);
+        replace(test3);
     }
 }
